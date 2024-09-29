1d74d04df18f40e46d3a9c4d515839fd7c90c0f9
courageousstrawberry <elech2a@gmail.com>
2024-09-29 09:37:08 -0700

    create setter function

--- colors_lab14.cpp
+++ colors_lab14.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
 class Color {
-
+    private:
+        int red;
+        int green;
+        int blue;
+    public:
+        void set_color(int r, int g, int b){
+            red = r;
+            green = g;
+            blue = b;
+        }
 };
