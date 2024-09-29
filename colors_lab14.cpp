d929f6e4cc76f19bbc394c65931cbc9b0129bea7
courageousstrawberry <elech2a@gmail.com>
2024-09-29 11:51:04 -0700

    add comments

--- colors_lab14.cpp
+++ colors_lab14.cpp
@@ -2,23 +2,31 @@
 #include <array>
 
 using namespace std;
+
+// Create color class.
 class Color {
     private:
+        // Three private integer colors to represent the color.
         int red;
         int green;
         int blue;
     public:
+        // Constructor for the class.
         Color(int r, int g, int b) {
+            // Use setter to initialize the object
             setColor(r,g,b);
         }
+        // Setter function to set each integer to it's corresponding private variables.
         void setColor(int r, int g, int b){
             red = r;
             green = g;
             blue = b;
         }
+        // Getter function that returns an array with each integer.
         array<int,3> getColor() const{
             return {red, green, blue};
         }
+        // Print function that displays the integer values of the color.
         void print(){
             cout << "The color is: " << endl;
             cout << red << ", " << green << ", " << blue << endl;
@@ -26,11 +34,13 @@ class Color {
 };
 
 int main() {
+    // Create four color objects.
     Color red(255,0,0);
     Color green(0,255,0);
     Color blue(0,0,255);
     Color orange(255,165,0);
 
+    // Display all four colors.
     cout << "Red" << endl;
     cout << "-----------" << endl;
     red.print();
